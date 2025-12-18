d6d1066a67ae4912f25ad4ec9179908d19a10793
dimitri.sluzki@haas.de <dimitri.sluzki@haas.de>
2025-12-18 16:10:49 +0100

    18.12.25  16:10

---
+++ dislMagicGarden/Platforms/Android/InAppUpdater.cs
@@ -0,0 +1,95 @@
+﻿#if ANDROID
+using Xamarin.Google.Android.Play.Core.AppUpdate;
+using Xamarin.Google.Android.Play.Core.Install.Model;
+using Xamarin.Google.Android.Play.Core.Tasks;
+
+namespace dislMagicGarden.Platforms.Android
+{
+    public class InAppUpdater
+    {
+        private const int UpdateRequestCode = 123;
+        private IAppUpdateManager _updateManager;
+
+        public async System.Threading.Tasks.Task CheckForUpdatesAsync()
+        {
+            try
+            {
+                var context = Platform.AppContext;
+                if (context == null)
+                {
+                    throw new InvalidOperationException("Android context is not available");
+                }
+
+                _updateManager = AppUpdateManagerFactory.Create(context);
+
+                // Get the update info task
+                var appUpdateInfoTask = _updateManager.AppUpdateInfo;
+
+                // Wait for the task to complete
+                var appUpdateInfo = await new PlayCoreTaskWrapper<AppUpdateInfo>(appUpdateInfoTask).GetAsync();
+
+                // Check if update is available
+                if (appUpdateInfo.UpdateAvailability() == UpdateAvailability.UpdateAvailable)
+                {
+                    // Check if the update is allowed
+                    // For immediate updates:
+                    if (appUpdateInfo.IsUpdateTypeAllowed(AppUpdateType.Immediate))
+                    {
+                        _updateManager.StartUpdateFlowForResult(
+                            appUpdateInfo,
+                            AppUpdateType.Immediate,
+                            Platform.CurrentActivity ?? throw new NullReferenceException("CurrentActivity is null"),
+                            UpdateRequestCode);
+                    }
+                    // For flexible updates:
+                    else if (appUpdateInfo.IsUpdateTypeAllowed(AppUpdateType.Flexible))
+                    {
+                        _updateManager.StartUpdateFlowForResult(
+                            appUpdateInfo,
+                            AppUpdateType.Flexible,
+                            Platform.CurrentActivity ?? throw new NullReferenceException("CurrentActivity is null"),
+                            UpdateRequestCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //if (ex != null)
+                //    SentrySdk.CaptureException(ex);
+                //throw;
+            }
+        }
+    }
+
+    public class PlayCoreTaskWrapper<T> : Java.Lang.Object, IOnSuccessListener, IOnFailureListener where T : class
+    {
+        private readonly TaskCompletionSource<T> _tcs = new();
+
+        public PlayCoreTaskWrapper(Xamarin.Google.Android.Play.Core.Tasks.Task task)
+        {
+            task.AddOnSuccessListener(this);
+            task.AddOnFailureListener(this);
+        }
+
+        public Task<T> GetAsync() => _tcs.Task;
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            if (result is T typedResult)
+            {
+                _tcs.TrySetResult(typedResult);
+            }
+            else
+            {
+                _tcs.TrySetException(new InvalidCastException($"Cannot cast {result?.GetType().Name} to {typeof(T).Name}"));
+            }
+        }
+
+        public void OnFailure(Java.Lang.Exception e)
+        {
+            _tcs.TrySetException(new Exception(e?.Message ?? "Unknown error in Play Core task"));
+        }
+    }
+}
+
+#endif
--- dislMagicGarden/Views/HomePage.xaml.cs
+++ dislMagicGarden/Views/HomePage.xaml.cs
@@ -4,7 +4,7 @@ namespace dislMagicGarden.Views;
 
 public partial class HomePage : FairyBasePage
 {
-  
+    static bool m_need_for_update = true;
 
     public HomePage(HomeViewModel vm)
 	{
@@ -12,4 +12,57 @@ public partial class HomePage : FairyBasePage
 
         BindingContext = vm;
     }
+
+    protected async override void OnAppearing()
+    {
+        if (m_need_for_update)
+        {
+#if ANDROID
+            if (IsPlayCoreApiAvailable())
+            {
+                await CheckForUpdates();
+            }
+#endif
+            m_need_for_update = false;
+        }
+    }
+        
+
+#if ANDROID
+        bool IsPlayCoreApiAvailable()
+    {
+        try
+        {
+            var context = Android.App.Application.Context;
+            var packageManager = context.PackageManager;
+            var playStorePackageName = "com.android.vending";
+            var intent = packageManager.GetLaunchIntentForPackage(playStorePackageName);
+            return intent != null;
+        }
+        catch
+        {
+            return false;
+        }
+
+
+    }
+
+
+    private async Task CheckForUpdates()
+    {
+        try
+        {
+            var updater = new Platforms.Android.InAppUpdater();
+            await updater.CheckForUpdatesAsync();
+        }
+        catch (Exception ex)
+        {
+
+            //SentrySdk.CaptureException(ex);
+            //await Shell.Current.DisplayAlert("Update Error", ex.Message, "OK");
+
+        }
+
+    }
+#endif
 }
